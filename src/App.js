701bf0e1fb75fcb8403c71235a779c9a260b7ef7
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2025-05-06 04:26:18 +0700

    3.resource-loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { UserInfo } from "./components/user-info";
-// import { CurrentUserLoader } from "./components/current-user-loader";
-import { UserLoader } from "./components/user-loader";
+//1. import { CurrentUserLoader } from "./components/current-user-loader";
+//2. import { UserLoader } from "./components/user-loader";
+import { ResourceLoader } from "./components/resource-loader";
 
 function App() {
-  return (
-    <>
-      <UserLoader userId={"1"}>
-        <UserInfo />
-      </UserLoader>
-    </>
-  );
+  return <>
+  <ResourceLoader resourceUrl={`/users/1`} resourceName="user">
+    <UserInfo />
+  </ResourceLoader>
+  </>;
 }
 
 export default App;
