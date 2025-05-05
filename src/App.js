09a4fc1ca30a2f38b552b6ccdb24b9c3497273ba
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2025-05-06 04:33:54 +0700

    4.data-source

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import axios from "axios";
+import { DataSource } from "./components/data-source";
 import { UserInfo } from "./components/user-info";
 //1. import { CurrentUserLoader } from "./components/current-user-loader";
 //2. import { UserLoader } from "./components/user-loader";
-import { ResourceLoader } from "./components/resource-loader";
+//3. import { ResourceLoader } from "./components/resource-loader";
+
+const getDataFromServer = async (url) => {
+  const response = await axios.get(`${url}`);
+  return response.data;
+};
 
 function App() {
-  return <>
-  <ResourceLoader resourceUrl={`/users/1`} resourceName="user">
-    <UserInfo />
-  </ResourceLoader>
-  </>;
+  return (
+    <>
+      <DataSource getData={()=>getDataFromServer("/users/2")} resourceName="user">
+        <UserInfo />
+      </DataSource>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/data-source.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export const DataSource = ({ getData = () => {}, resourceName, children }) => {
+  const [resource, setResource] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getData();
+      setResource(response);
+    })();
+  }, [getData]);
+
+  return (
+    <>
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, { [resourceName]: resource });
+        }
+        return child;
+      })}
+    </>
+  );
+};
